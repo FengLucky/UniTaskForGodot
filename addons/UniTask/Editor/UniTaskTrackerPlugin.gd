1fdab5d2253554133aa44337bf05c5205c358183
luckyfeng <1101458726@qq.com>
2025-05-01 23:44:52 +0800

    修复编辑器工具多语言未生效

--- addons/UniTask/Editor/UniTaskTrackerPlugin.gd
+++ addons/UniTask/Editor/UniTaskTrackerPlugin.gd
@@ -3,34 +3,24 @@ const UniTaskTrackerDebugger = preload("res://addons/UniTask/Editor/UniTaskTrack
 
 var debugger: UniTaskTrackerDebugger = null;
 
-func _enter_tree() -> void:
-	if !Engine.is_editor_hint():
-		return;
-	
-	debugger = UniTaskTrackerDebugger.new();
-	add_debugger_plugin(debugger);
-	
+func _enter_tree() -> void:		
 	var dir_path: String = "res://addons/UniTask/Translations/";
 	var dir: DirAccess = DirAccess.open(dir_path);
 	if dir != null:
 		dir.list_dir_begin();
+		var domain = TranslationServer.get_or_add_domain("godot.editor");
 		var path: String = dir.get_next();
 		while path != null && path != "":
 			if(path.ends_with(".translation")):
 				var res = load(dir_path +"/"+ path);
-				TranslationServer.add_translation(res);
+				domain.add_translation(res);
 			path = dir.get_next();
 		dir.list_dir_end();
-	TranslationServer.set_locale("zh_CN")	
-	TranslationServer.set_translation_domain("zh_CN")
-	print(TranslationServer.get_language_name("zh"))
-	print(TranslationServer.translate("UniTask_Clear"))
-	print(TranslationServer.get_translation_domain())
-	print(EditorInterface.can_translate_messages())
+	
+	debugger = UniTaskTrackerDebugger.new();
+	add_debugger_plugin(debugger);	
 	
 func _exit_tree() -> void:
-	if !Engine.is_editor_hint():
-		return;
 	if self.debugger != null:
 		remove_debugger_plugin(debugger);
 		self.debugger = null;	
